3e3ad27cf1b6127c50f3b5db49cfcb9c544ea0ef
Anshuman Jha <anshumanjha1995@hotmail.com>
2018-03-02 09:17:55 +0530

    Create SN_record-clustering.py
    Algorithm 2 -sliding window connection

---
+++ SN_record-clustering.py
@@ -0,0 +1,10 @@
+def slidingWindow(self, win_size):
+	#Algorithm2 -sliding window
+	#input: sliding window size(winSize)
+	#create all candidate pairs and forward each to phase 2 of cocktail mod1/2
+	bdSize = len(self.sorted_list)
+	totalSlides = bdSize - winSize
+	
+	for i in range(0, TotalSlides):
+		ids = self.sorted_list[i:i+win_size]
+		self.pairRecords_slide(ids)
