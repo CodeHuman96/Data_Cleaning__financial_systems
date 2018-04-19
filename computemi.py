4d3e4e0b8c7c12ded768a2d621b8f4fb33d1274e
Anshuman Jha <anshumanjha1995@hotmail.com>
2018-04-20 00:08:16 +0530

    Update computemi.py

--- computemi.py
+++ computemi.py
@@ -1,3 +1,14 @@
 def computeMI(data)
   m = 0.9
-  u =
+  u = 0.1
+  p = 0.5
+  convergeValue = 0.00001
+  for i in data:
+    m[i] = m
+    u[i] = u
+  while m[i+1]-m[i] !=convergeValue 
+             and 
+        u[i+1]-u[i] !=convergeValue:  
+        runEStep(m[i],u[i],p)
+return        
+        
